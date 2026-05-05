079446bec142ef50a7c4a233278bdd6f39786491
dimrastur-wq <dimrastur@gmail.com>
2026-05-05 10:19:59 +0500

    Updated sitemap.xml.ts

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,4 +1,5 @@
 import { CITIES, SERVICES } from '../data/services';
+import { BLOG_ARTICLES } from '../data/blog';
 
 const siteUrl = 'https://master-tehniki74.store';
 
@@ -8,10 +9,15 @@ export async function GET() {
   const today = new Date().toISOString().split('T')[0];
 
   pages.push({ loc: `${siteUrl}/`, changefreq: 'weekly', priority: '1.0', lastmod: today });
+  pages.push({ loc: `${siteUrl}/blog/`, changefreq: 'weekly', priority: '0.9' });
   pages.push({ loc: `${siteUrl}/zapchasti/`, changefreq: 'weekly', priority: '0.8' });
   pages.push({ loc: `${siteUrl}/tseny/`, changefreq: 'monthly', priority: '0.8' });
   pages.push({ loc: `${siteUrl}/b2b/`, changefreq: 'monthly', priority: '0.7' });
   pages.push({ loc: `${siteUrl}/admin/`, changefreq: 'monthly', priority: '0.3' });
+
+  for (const article of BLOG_ARTICLES) {
+    pages.push({ loc: `${siteUrl}/blog/${article.slug}/`, changefreq: 'monthly', priority: '0.7', lastmod: article.date });
+  }
 
   for (const city of Object.values(CITIES)) {
     pages.push({ loc: `${siteUrl}/${city.slug}/`, changefreq: 'weekly', priority: '0.9' });
